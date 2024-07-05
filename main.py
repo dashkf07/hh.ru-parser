cc23b623760a6cb5add94430770f0d97bbe1f949
dashkf07 <dashk.f07@gmail.com>
2024-07-06 01:06:21 +0300

    next page with vacancies function

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from parser import parser
+from selenium import webdriver
+import asyncio
 
 
-def main():
-    parser.get_html_by_key_words()
+driver = webdriver.Firefox()
+find_url = 'https://hh.ru/'
+keys = 'разработчик java'
+
+
+async def main():
+    data = await parser.get_html_by_key_words(driver, keys)
+    new_data = await parser.get_more_html_by_key_words(driver, data.get('url'))
 
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
+
--- parser/parser.py
+++ parser/parser.py
@@ -11,9 +11,9 @@ find_filter_url = 'https://hh.ru/search/vacancy/advanced?hhtmFrom=main'
 find_url = 'https://hh.ru/'
 
 
-def get_html_by_key_words():
-    # open browser
-    driver = webdriver.Firefox()
+
+
+async def get_html_by_key_words(driver, key_words):
 
     # page
     driver.get(find_url)
@@ -21,11 +21,56 @@ def get_html_by_key_words():
     # get input element
     inp = driver.find_element(By.NAME, 'text')
     inp.clear()
-    inp.send_keys('разработчик java')
+    inp.send_keys(key_words)
 
     # get submit button element
     submit_button = driver.find_element(By.CSS_SELECTOR, "button[data-qa='search-button']")
     submit_button.click()
+
+    wait = WebDriverWait(driver, 500)
+    wait.until(EC.visibility_of_element_located((By.ID, "a11y-main-content")))
+
+    soup = BeautifulSoup(driver.page_source, 'lxml')
+
+    vacancies = soup.find_all('div', class_='vacancy-card--z_UXteNo7bRGzxWVcL7y')
+
+    vacancies_data = []
+    for vacancy in vacancies:
+        vacancy_name = vacancy.find('span', class_='vacancy-name--c1Lay3KouCl7XasYakLk').get_text().replace('\xa0', ' ').replace('\u202f', ' ')
+        vacancy_salary = (vacancy.find('div', class_='compensation-labels--uUto71l5gcnhU2I8TZmz')
+                          .find('span', 'fake-magritte-primary-text--Hdw8FvkOzzOcoR4xXWni'))
+        if (vacancy_salary == None):
+            vacancy_salary = 'Не указано'
+        else:
+            vacancy_salary = vacancy_salary.get_text().replace('\xa0', ' ').replace('\u202f', ' ')
+
+        vacancy_experience = vacancy.find('span', class_='label--rWRLMsbliNlu_OMkM_D3').get_text()
+        vacancy_company = vacancy.find('span', class_='company-info-text--vgvZouLtf8jwBmaD1xgp').get_text().replace('\xa0', ' ').replace('\u202f', ' ')
+
+        vacancy_city = ((vacancy.find(attrs={"data-qa": "vacancy-serp__vacancy-address"}))
+                          .find('span', class_='fake-magritte-primary-text--Hdw8FvkOzzOcoR4xXWni')).get_text().replace('\xa0', ' ').replace('\u202f', ' ')
+
+        vacancies_data.append({
+            'name': vacancy_name,
+            'salary': vacancy_salary,
+            'experience': vacancy_experience,
+            'company': vacancy_company,
+
+        })
+
+    return {
+        'vacancies_data': vacancies_data,
+        'url': driver.current_url
+    }
+
+
+async def get_more_html_by_key_words(driver, url):
+
+    driver.get(url)
+
+    next_button_element = driver.find_element(By.CSS_SELECTOR, "a[data-qa='pager-next']")
+    driver.execute_script("arguments[0].click();", next_button_element)
+
 
     wait = WebDriverWait(driver, 500)
     wait.until(EC.visibility_of_element_located((By.ID, "a11y-main-content")))
@@ -67,7 +112,11 @@ def get_html_by_key_words():
 
         })
 
-    driver.quit()
+    return {
+        'vacancies_data': vacancies_data,
+        'url': driver.current_url
+    }
+
 
 
 
